257d2f9149571381ee2da1d64aa938ad6b76c72e
Michael Méndez <michael.mendez@avantica.net>
2020-04-11 20:14:28 -0600

    Initial commit

---
+++ pet-clinic-data/src/main/java/com/codenexus/petclinic/model/Owner.java
@@ -0,0 +1,7 @@
+package com.codenexus.petclinic.model;
+
+public class Owner extends Person {
+    public Owner() {
+        super();
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/codenexus/petclinic/model/PetType.java
@@ -0,0 +1,17 @@
+package com.codenexus.petclinic.model;
+
+public class PetType {
+    private String name;
+
+    public PetType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
